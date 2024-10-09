3dcb0d6c72fbfadf4bcbfe1f218e7be7207461aa
gaopeng71 <gaopeng8@jd.com>
2024-10-09 17:10:34 +0800

    抑制 inspect 的日志输出

--- internal/fs/templates/FederatedIndirectRiskDetector.java
+++ internal/fs/templates/FederatedIndirectRiskDetector.java
@@ -24,7 +24,7 @@ public class FederatedIndirectRiskDetector {
     public void detectRisks() throws IOException {
         String classpath = System.getProperty("java.class.path");
         String[] classpathEntries = classpath.split(File.pathSeparator);
-        log.info("Starting risk detection with classpath: {}, {}", classpath, classpathEntries);
+        log.info("Starting risk detection ...");
         for (String entry : classpathEntries) {
             if (entry.endsWith(".jar")) {
                 processJar(entry);
@@ -39,7 +39,7 @@ public class FederatedIndirectRiskDetector {
     }
 
     private void processJar(String jarPath) throws IOException {
-        log.debug("processing {}", jarPath);
+        log.debug("Processing {}", jarPath);
         try (JarFile jarFile = new JarFile(jarPath)) {
             Enumeration<JarEntry> entries = jarFile.entries();
             URL[] urls = { new URL("jar:file:" + jarPath + "!/") };
