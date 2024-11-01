f544ebe4e348391a5aa174db73f06091fafe85f0
gaopeng71 <gaopeng8@jd.com>
2024-11-01 13:54:39 +0800

    n

---
+++ pkg/merge/property_analyze.go
@@ -0,0 +1,232 @@
+package merge
+
+import (
+	"bufio"
+	"fmt"
+	"io/fs"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"federate/pkg/manifest"
+	"federate/pkg/tablerender"
+	"github.com/fatih/color"
+	"gopkg.in/yaml.v2"
+)
+
+func (cm *PropertyManager) analyzePropertiesFile(filePath string, component manifest.ComponentInfo) error {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return cm.handleFileErr(component.Name, filePath, err)
+	}
+	defer file.Close()
+
+	log.Printf("[%s] Processing %s", component.Name, filePath)
+
+	properties := make(map[string]interface{})
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if line == "" || strings.HasPrefix(line, "#") {
+			continue
+		}
+		parts := strings.SplitN(line, "=", 2)
+		if len(parts) == 2 {
+			key := strings.TrimSpace(parts[0])
+			value := strings.TrimSpace(parts[1])
+			properties[key] = value
+		}
+	}
+
+	if err = scanner.Err(); err != nil {
+		return err
+	}
+
+	mergeMaps(cm.mergedProperties, properties, cm.propertiesConflictKeys, cm, component)
+	return nil
+}
+
+func (cm *PropertyManager) analyzeYamlFile(filePath string, springProfile string, component manifest.ComponentInfo) error {
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		return cm.handleFileErr(component.Name, filePath, err)
+	}
+
+	log.Printf("[%s:%s] Processing %s", component.Name, springProfile, filePath)
+
+	dataStr := string(data)
+	dataStr = strings.ReplaceAll(dataStr, springProfileActive, springProfile)
+
+	var config map[interface{}]interface{}
+	if err = yaml.Unmarshal([]byte(dataStr), &config); err != nil {
+		return err
+	}
+
+	flatConfig := make(map[string]interface{})
+	cm.flattenYamlMap(config, "", flatConfig)
+
+	mergeMaps(cm.mergedYaml, flatConfig, cm.yamlConflictKeys, cm, component)
+
+	// 处理 spring.profiles.include
+	if includes, ok := flatConfig[springProfileInclude]; ok {
+		if includeProfiles, ok := includes.(string); ok {
+			for _, includeProfile := range strings.Split(includeProfiles, ",") {
+				includeProfile = strings.TrimSpace(includeProfile)
+				log.Printf("[%s:%s] Detected spring.profiles.include: %s", component.Name, springProfile, includeProfile)
+				if includeProfile != "" {
+					cm.analyzeYamlFile(filepath.Join(filepath.Dir(filePath), "application-"+includeProfile+".yml"), includeProfile, component)
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
+func (cm *PropertyManager) handleFileErr(componentName, filePath string, err error) error {
+    if _, ok := err.(*fs.PathError); ok {
+        return nil
+    }
+    return err
+}
+
+func (cm *PropertyManager) recordConflict(conflictMap map[string]map[string]interface{}, key, componentName string, value interface{}) {
+	if _, exists := conflictMap[key]; !exists {
+		conflictMap[key] = make(map[string]interface{})
+	}
+	conflictMap[key][componentName] = trimValue(value)
+}
+
+func (cm *PropertyManager) identifyPropertyConflicts(conflictKeys map[string]map[string]interface{}) map[string]map[string]interface{} {
+	filteredConflicts := make(map[string]map[string]interface{})
+	for key, components := range conflictKeys {
+		if len(components) == 1 {
+			// 如果只在一个组件中出现，不算冲突
+			continue
+		}
+
+		uniqueValues := make(map[string]bool)
+		for _, value := range components {
+			if !isEmptyValue(value) {
+				uniqueValues[fmt.Sprintf("%v", value)] = true
+			}
+		}
+
+		if len(uniqueValues) > 1 {
+			filteredConflicts[key] = components
+		}
+	}
+	return filteredConflicts
+}
+
+func (cm *PropertyManager) flattenYamlMap(data map[interface{}]interface{}, parentKey string, result map[string]interface{}) {
+	for k, v := range data {
+		fullKey := strings.TrimPrefix(fmt.Sprintf("%s.%v", parentKey, k), ".")
+		switch vTyped := v.(type) {
+		case map[interface{}]interface{}:
+			cm.flattenYamlMap(vTyped, fullKey, result)
+		default:
+			result[fullKey] = v
+		}
+	}
+}
+
+func (cm *PropertyManager) unflattenYamlMap(data map[string]interface{}) map[string]interface{} {
+	result := make(map[string]interface{})
+	for key, value := range data {
+		keys := strings.Split(key, ".")
+		currentMap := result
+		for i, k := range keys {
+			if i == len(keys)-1 {
+				currentMap[k] = value
+			} else {
+				if _, ok := currentMap[k]; !ok {
+					currentMap[k] = make(map[string]interface{})
+				}
+				currentMap = currentMap[k].(map[string]interface{})
+			}
+		}
+	}
+	return result
+}
+
+func mergeMaps(dest, src map[string]interface{}, conflictMap map[string]map[string]interface{}, cm *PropertyManager, component manifest.ComponentInfo) {
+	for k, v := range src {
+		if !cm.isReservedProperty(k) {
+			cm.registerPropertyValue(conflictMap, k, component.Name, v)
+		} else {
+			cm.registerReservedProperty(k, component, v)
+		}
+
+		dest[k] = v
+	}
+}
+
+// 记录值并检查冲突
+func (cm *PropertyManager) registerPropertyValue(conflictMap map[string]map[string]interface{}, key, componentName string, value interface{}) {
+	if _, exists := conflictMap[key]; !exists {
+		conflictMap[key] = make(map[string]interface{})
+	}
+	conflictMap[key][componentName] = value
+}
+
+func (cm *PropertyManager) registerReservedProperty(key string, component manifest.ComponentInfo, value interface{}) {
+	if _, exists := cm.reservedValues[key]; !exists {
+		cm.reservedValues[key] = []ComponentKeyValue{}
+	}
+	cm.reservedValues[key] = append(cm.reservedValues[key], ComponentKeyValue{Component: component, Value: value})
+}
+
+func (cm *PropertyManager) applyReservedPropertyRules() {
+	var cellData [][]string
+	for key, values := range cm.reservedValues {
+		if handler, exists := cm.reservedYamlKeys[key]; exists {
+			if cm.m.Main.Reconcile.PropertySettled(key) {
+				color.Yellow("key:%s reserved, but used directive: propertySettled, skipped", key)
+				continue
+			}
+			if value := handler(cm, values); value != nil {
+				cm.mergedYaml[key] = value
+				cellData = append(cellData, []string{key, fmt.Sprintf("%v", value)})
+			} else {
+				delete(cm.mergedYaml, key)
+				cellData = append(cellData, []string{key, color.New(color.FgRed).Add(color.CrossedOut).Sprintf("deleted")})
+			}
+		}
+	}
+
+	header := []string{"Reserved Key", "Value"}
+	tablerender.DisplayTable(header, cellData, false, -1)
+}
+
+func isEmptyValue(value interface{}) bool {
+	switch v := value.(type) {
+	case nil:
+		return true
+	case string:
+		return v == ""
+	case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64, float32, float64:
+		return v == 0
+	case bool:
+		return !v
+	case []interface{}:
+		return len(v) == 0
+	case map[string]interface{}:
+		return len(v) == 0
+	default:
+		return false
+	}
+}
+
+func trimValue(value interface{}) interface{} {
+	if str, ok := value.(string); ok {
+		return strings.TrimSpace(str)
+	}
+	return value
+}
+
+func (cm *PropertyManager) isReservedProperty(key string) bool {
+	_, exists := cm.reservedYamlKeys[key]
+	return exists
+}
