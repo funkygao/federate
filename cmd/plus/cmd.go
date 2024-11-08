62c1f5344f7f049fae8b8368c0be4c5d8073baef
gaopeng71 <gaopeng8@jd.com>
2024-11-08 19:32:04 +0800

    n

--- cmd/plus/cmd.go
+++ cmd/plus/cmd.go
@@ -6,7 +6,7 @@ import (
 
 var CmdGroup = &cobra.Command{
 	Use:   "wms-plus",
-	Short: "Develop new and tailor existing capabilities atop WMS6",
+	Short: "Develop new and tailor capabilities atop WMS6",
 	Long: `Create new functionalities and adaptations leveraging WMS6, without modifying the core platform.
 
   Find more information at: http://w6-developer.jdl.com/`,
