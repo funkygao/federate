47da8f037f9b43ffef327451fd8dd11633f39779
gaopeng71 <gaopeng8@jd.com>
2024-12-03 13:56:04 +0800

    n

--- pkg/merge/property/task.go
+++ pkg/merge/property/task.go
@@ -88,7 +88,7 @@ func (t *reconcileTask) namespaceKeyReferences(fileFilter func(os.FileInfo, stri
 					replaced := t.replaceKeyInMatch(match, t.keys[i], newKey)
 					dmp := diffmatchpatch.New()
 					diffs := dmp.DiffMain(match, replaced, false)
-					log.Printf("Transforming %s: %s, %s", path, dmp.DiffPrettyText(diffs), regex.String())
+					log.Printf("Transforming %s\n%s", path, dmp.DiffPrettyText(diffs))
 					return replaced
 				})
 				if strings.Contains(regex.String(), "@ConfigurationProperties") {
