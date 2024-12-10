519b6d3f7822c6dbb1d1b8e9a8708513be65f2b8
gaopeng71 <gaopeng8@jd.com>
2024-12-10 15:11:25 +0800

    FIX: i18n message bundles not found

--- pkg/primitive/primitive_test.go
+++ pkg/primitive/primitive_test.go
@@ -1,8 +1,21 @@
 package primitive
 
 import (
+	"sort"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestPlaceholder(t *testing.T) {
+func TestValues(t *testing.T) {
+	s := NewStringSet()
+	s.Add("2")
+	s.Add("1")
+	s.Add("2")
+	s.Add("5")
+	assert.Equal(t, 3, s.Size())
+
+	values := s.Values()
+	sort.Strings(values)
+	assert.Equal(t, []string{"1", "2", "5"}, values)
 }
