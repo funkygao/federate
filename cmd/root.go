d044dd0a93c511f7a4cee1ef626783c640f91fed
Funky Gao <funky.gao@gmail.com>
2024-12-14 00:15:04 +0800

    n

--- cmd/root.go
+++ cmd/root.go
@@ -29,8 +29,6 @@ var rootCmd = &cobra.Command{
 `,
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		os.Exit(1)
