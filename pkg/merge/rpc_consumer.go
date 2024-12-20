baf93b3b034c56777a5ad25782736e4f2fdc9aa5
gaopeng71 <gaopeng8@jd.com>
2024-12-20 15:56:22 +0800

    n

--- pkg/merge/rpc_consumer.go
+++ pkg/merge/rpc_consumer.go
@@ -53,7 +53,7 @@ func (dm *RpcConsumerManager) referenceXmlTags() []string {
 }
 
 func (dm *RpcConsumerManager) Name() string {
-	return fmt.Sprintf("Merge %s Consumer XML to avoid redundant resource consumption", dm.rpcType)
+	return fmt.Sprintf("Merge %s Consumer XML to avoid remote resource consumption", dm.rpcType)
 }
 
 func (dm *RpcConsumerManager) RPC() string {
