84ae68cec7172e9051958fa3567282dc19856d3b
Funky Gao <funky.gao@gmail.com>
2025-01-13 23:51:59 +0800

    n

--- cmd/insight/cmd.go
+++ cmd/insight/cmd.go
@@ -10,5 +10,5 @@ var CmdGroup = &cobra.Command{
 }
 
 func init() {
-	CmdGroup.AddCommand(astCmd, taxonomyCmd, extensionCmd, mybatisCmd, gitInsightCmd, tfidfCmd)
+	CmdGroup.AddCommand(astCmd, mybatisCmd, gitInsightCmd, taxonomyCmd, extensionCmd, tfidfCmd)
 }
